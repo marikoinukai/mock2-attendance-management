708e7be45df4bda6a9b552c25ef3c64cdf990b53
mariko inukai <mariko.inukai1006@gmail.com>
2026-05-31 11:15:36 +0900

    Create attendance index page

---
+++ src/resources/views/attendance/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>勤怠登録</title>
+</head>
+
+<body>
+    <h1>勤怠登録画面</h1>
+
+    <p>ログイン・メール認証済みユーザーだけが見られる画面です。</p>
+</body>
+
+</html>
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AttendanceController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/attendance', [AttendanceController::class, 'index'])->name('attendance.index');
 });
