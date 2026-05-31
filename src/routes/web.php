b2c3cb5837fea3870865339b576dfc5515272da0
mariko inukai <mariko.inukai1006@gmail.com>
2026-05-31 18:22:10 +0900

    Fix admin route middleware

--- src/routes/web.php
+++ src/routes/web.php
@@ -23,20 +23,38 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+/*
+|--------------------------------------------------------------------------
+| 管理者ログイン
+|--------------------------------------------------------------------------
+*/
+Route::get('/admin/login', [AdminAuthController::class, 'showLogin'])->name('admin.login.show');
+Route::post('/admin/login', [AdminAuthController::class, 'login'])->name('admin.login');
+
+/*
+|--------------------------------------------------------------------------
+| 一般ユーザー
+|--------------------------------------------------------------------------
+*/
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/attendance', [AttendanceController::class, 'index'])->name('attendance.index');
     Route::post('/attendance/clock-in', [AttendanceController::class, 'clockIn'])->name('attendance.clock_in');
     Route::post('/attendance/clock-out', [AttendanceController::class, 'clockOut'])->name('attendance.clock_out');
     Route::post('/attendance/break-start', [AttendanceController::class, 'breakStart'])->name('attendance.break_start');
     Route::post('/attendance/break-end', [AttendanceController::class, 'breakEnd'])->name('attendance.break_end');
+
     Route::get('/attendance/list', [AttendanceListController::class, 'index'])->name('attendance.list');
     Route::get('/attendance/detail/{id}', [AttendanceDetailController::class, 'show'])->name('attendance.detail');
     Route::post('/attendance/detail/{id}/correction', [AttendanceCorrectionRequestController::class, 'store'])->name('attendance.correction.store');
-    Route::get('/stamp_correction_request/list', [AttendanceCorrectionRequestController::class, 'index'])->name('attendance_correction_requests.index');
-    Route::get('/admin/login', [AdminAuthController::class, 'showLogin'])->name('admin.login.show');
-    Route::post('/admin/login', [AdminAuthController::class, 'login'])->name('admin.login');
 
-    Route::middleware(['auth', 'verified', 'admin'])->group(function () {
-        Route::get('/admin/attendance/list', [AdminAttendanceController::class, 'index'])->name('admin.attendance.index');
-    });
+    Route::get('/stamp_correction_request/list', [AttendanceCorrectionRequestController::class, 'index'])->name('attendance_correction_requests.index');
+});
+
+/*
+|--------------------------------------------------------------------------
+| 管理者
+|--------------------------------------------------------------------------
+*/
+Route::middleware(['auth', 'verified', 'admin'])->group(function () {
+    Route::get('/admin/attendance/list', [AdminAttendanceController::class, 'index'])->name('admin.attendance.index');
 });
